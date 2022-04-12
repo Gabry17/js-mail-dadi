8e57787b08a693135cc3dc2e0c0b7a290b4be31c
Gabry17 <gbianchi528@gmail.com>
2022-04-12 15:02:47 +0200

    chiesto all utente di inserire mail controllato che sia presente nella lista e aggiunto esito controllo

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,1 +1,28 @@
-alert('ciao');
+//DICHIARAZIONE
+//chiedere mail all utente
+const addMail = prompt('inserire la sua mail:');
+console.log(addMail);
+
+//ESECUZIONE
+//controllo che la mail e presente nella lista
+//lista
+const listMail = ['mariorossi@gmail.com','gabrielebianchi@gmail.com','topolino@gmail.com','pincopallo@gmail.com'];
+
+//controllo
+let control = false;
+
+for(let i = 0; i < listMail.length; i++){
+    let list = listMail[i];
+    if (addMail === list) {
+        control = true;
+        console.log(control);
+    }
+}
+
+//OUTPUT
+//stampare messaggio secondo esito del controllo
+if (control ===  true) {
+    console.log('Bentornato amico!');
+} else{
+    alert('L\'account selezionato non è presente sul nostro sistema');
+}

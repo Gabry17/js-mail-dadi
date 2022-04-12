822c9eef5671a6b2c2fd7301dd706ee1e8688ecb
Gabry17 <gbianchi528@gmail.com>
2022-04-12 15:25:55 +0200

    generato due numeri da 1 a 6 stabilito numero piu alto = vincitore

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -1,1 +1,19 @@
-alert('fjfjfjj');
+//DICHIARAZIONE
+//generare numero a caso da 1 a 6 per giocatore
+const numPlayer = Math.floor(Math.random() * 6) + 1;
+console.log(numPlayer);
+//generare numero a caso da 1 a 6 per computer
+const numPc = Math.floor(Math.random() * 6) + 1;
+console.log(numPc);
+
+//ELABORAZIONE
+//se giocatore  piu alto vince
+//se computer e piu alto vince
+//se giocatore e computer sono uguali pareggio
+if (numPlayer > numPc ) {
+    console.log('player vince');
+} else if (numPc > numPlayer) {
+    console.log('pc vince');
+} else {
+    console.log('pareggio');
+}
